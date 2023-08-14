391902f36b963ac577abf8e0aa3c3929ebfb54f2
JorG1990 <jorge  garcia>
2023-08-14 16:36:57 -0500

    creacion2

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -3,15 +3,9 @@ import PropTypes from "prop-types";
 import { Button } from "./FeedbackOptions.styled";
 
 const FeedbackOptions =({options, onLeaveFeedeback}) => {
-  const feedbackOptions = [
-    { value: 'good', label: 'Good' },
-    { value: 'neutral', label: 'Neutral' },
-    { value: 'bad', label: 'Bad' },
-  ];
-
   return (
     <div>
-      {feedbackOptions.map((option) => (
+      {options.map((option) => (
         <Button type="button" key={option.value} onClick={() => onLeaveFeedeback(option.value)}>
           {option.label}
         </Button>
